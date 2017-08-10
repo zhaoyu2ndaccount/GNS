3c1ec8e9a03f8c9c06010ffdca61dd18325d4874
V. Arun <arun@cs.umass.edu>
2017-08-10 17:39:56 -0400

    Temp commit from Travis script

--- scripts/travis_checks.sh
+++ scripts/travis_checks.sh
@@ -1,109 +1,41 @@
 #!/usr/bin/env bash
 
+echo 0 > "successes"
+echo 0 > "failures"
 
-# This script acts as a "status check" in Travis.
-# This script will be run when a pull request is created and when something is committed.
-# If this script fails, the pull request is flagged accordingly and cannot be merged.
+i=0
+while true; do
+	
+head_check
+err_status=$?
 
-# Usage travis_checks.sh [--update]
-# --update will update warning counts instead of checking it
-
-
-# If the script is invoked from a different directory,
-# adjust pwd accordingly
-cd $(dirname $0)
-script_dir=`pwd`
-
-
-# Run ant test 10 times
-# A single failure will flag a Travis build failure
-test_repeat=7 # Temporary change, replace with 10 ASAP
-
-# The warning_counts file keeps track of previous warning counts
-wcount_file="warning_counts"
-
-# Run with --update to update warning counts
-update_option=--update
-
-# Warning count file should exist. Otherwise, run with --update
-if [ ! -f  $wcount_file ] &&  [[ $* != *$update_option* ]]; then
-    echo "warning count file not found, run the script with $update_option to reset counts. Exiting.."
-    exit 1
+successes=`cat successes`
+failures=`cat failures`
+if [[ $err_status == 0 ]]; then
+  successes=`expr $successes + 1`
+else 
+  failures=`expr $failures + 1`
 fi
+i=`expr $i + 1`
+echo $successes > "successes"
+echo $failures > "failures"
+echo "===Iteration $i, successes=$successes, failures=$failures==="
+done &
 
+echo "Background monitoring started at `date`.."
+# Travis gives 49 minutes exactly. ant takes ~1:30. 30 minutes buffer. So, 47 minutes = 2820 seconds.
+sleep 2820
 
-# If warning count file is present, ensure that it is sane
-if [[ $* != *$update_option* ]]; then
-	wfile_line_count=`cat $wcount_file|wc -l`
-	if [ $wfile_line_count -ne 1 ] ; then
-		echo "Invalid warning count file, exiting.."
-		exit 1
-	fi
-
-# A count of the number of warnings we get in compiling sources
-# The idea is to never exceed this as more commits are added
-
-	javac_src_warning_count=`cat $wcount_file|head -n 1|tail -n 1`
-fi
-
-# Switch to the project root
-cd ..
-project_root=`pwd`
-
-# Always start clean, otherwise some files won't be compiled
-ant clean
-
-# Captures:
-# [javac] 1069 problems (1069 warnings) 
-# or 
-# [javac] 1070 problems (1 error, 1069 warnings)
-
-result_javac=`ant compile_eclipse -lib ./lib | tee /dev/tty | grep -iE "[0-9]+ (errors?|warnings?)"`
-
-if echo "$result_javac"|grep -qiE  "[0-9]+ warnings?"; then
-	javac_wcount=`echo "$result_javac"|grep -iEo "[0-9]+ warnings?"|grep -iEo "[0-9]+"`
+successes=`cat successes`
+failures=`cat failures`
+echo "=== Summary: successes=$successes, failures=$failures ==="
+if [ $successes -eq 0 ] && [ $failures -eq 0 ]; then
+	echo "No progress, something went wrong.."
+	exit 1
+elif [ $failures -gt 0 ]; then
+	echo "There are failures, exiting with 1.."
+	exit 1
 else
-	javac_wcount="0";
-fi
-
-
-if echo "$result_javac"|grep -qiE  "[0-9]+ errors?"; then
-	javac_ecount=`echo "$result_javac"|grep -iEo "[0-9]+ errors?"|grep -iEo "[0-9]+"`
-else
-	javac_ecount="0";
-fi
-
-
-
-echo $javac_wcount " java warning(s)"
-echo $javac_ecount " java error(s)"
-
-if [[ $* == *$update_option* ]]; then
-	cd `echo $script_dir`
-	echo "Writing counts to $wcount_file"
-	echo "$javac_wcount" > `echo $wcount_file`
-	echo "Warning count updated to $javac_wcount"
+	echo "All good, exiting with 0"
 	exit 0
 fi
-
-if [ "$javac_ecount" -gt "0" ]; then
-	echo "java error count has increased from 0 to $javac_ecount. You have introduced additional errors. Exiting with failure.."
-	exit 1
-fi
-
-if [ "$javac_wcount" -gt "$javac_src_warning_count" ]; then
-	echo "java warning count has increased from $javac_src_warning_count to $javac_wcount. You have introduced additional warnings. Exiting with failure.."
-	exit 1
-elif [ "$javac_wcount" -lt "$javac_src_warning_count" ]; then
-	echo "Warning count has reduced, consider updating the warning count file using $update_option option"
-fi
-
-
-# Do a complete build and generate jars
-ant
-
-# Run ant test multiple times
-
-for ((i=0; i<=$test_repeat; i++)); do
-   ant test || { echo "Test $i failed, exiting.." ; exit 1; }
-done
